6f8024bf97967aa954434db4b3cce0294b5b90f9
Kevin Egan <kevinegan31@gmail.com>
2023-10-18 10:27:57 -0500

    generating package

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,2 @@
-library(testthat)
 library(ARGOS)
-# testthat::test_check("ARGOS")
-devtools::test("ARGOS")
-
-test_files <- testthat::test_dir("tests/testthat/")
-print(test_files)
+devtools::test()
