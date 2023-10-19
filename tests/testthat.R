a6dc96375d7c50c8d6a97f23adcc2f1e96e826cb
Kevin Egan <kevinegan31@gmail.com>
2023-10-19 10:53:24 -0500

    Updating tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,7 @@
-library(ARGOS)
-devtools::test()
+# library(ARGOS)
+# devtools::load_all(".")
+# devtools::test()
 # devtools::test("tests/testthat")
+library(testthat)
+library(ARGOS)  # Your package
+test_check("ARGOS")  # Your package name
