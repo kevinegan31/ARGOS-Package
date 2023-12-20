fdd54273b4ba670b7de51a2f60ecdd266087b612
Kevin Egan <kevinegan31@gmail.com>
2023-12-20 12:19:16 -0600

    Update python_load.R

--- R/python_load.R
+++ R/python_load.R
@@ -1,5 +1,18 @@
+# #' @importFrom reticulate source_python
+# .onLoad <- function(libname, pkgname) {
+#   python_script <- system.file("dynamical_systems.py", package = pkgname, mustWork = TRUE)
+#   reticulate::source_python(python_script)
+# }
+
 #' @importFrom reticulate source_python
 .onLoad <- function(libname, pkgname) {
+  # Source the Python script
   python_script <- system.file("dynamical_systems.py", package = pkgname, mustWork = TRUE)
   reticulate::source_python(python_script)
+
+  # Check for and possibly install 'scipy'
+  if (!reticulate::py_module_available("scipy")) {
+    packageStartupMessage("The 'scipy' Python module is required but not available. Attempting to install 'scipy'.")
+    reticulate::py_install("scipy")
+  }
 }
